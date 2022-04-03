8c4d8a4286d284bbf160596217974aaa9bfcdc7e
BlackRaven0405 <ljouhault@crowbots.shop>
2022-04-03 21:16:10 +0200

    update python requirement in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 import re
 
-version = ''
 with open('gogame/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError('version is not set')
 
-readme = ''
 with open('README.rst') as f:
     readme = f.read()
 
@@ -24,9 +22,8 @@ setup(
     packages=['gogame'],
     url='https://github.com/BlackRaven0405/gogame',
     keywords='go gogame',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
           'numpy',
       ],
-
 )
