3e5a7067f6466ae2a43e3d96b56be5f81cd016a9
BlackRaven0405 <ljouhault@crowbots.shop>
2022-04-09 15:01:53 +0200

    First complete release

--- gogame/__init__.py
+++ gogame/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'gogame'
 __author__ = 'BlackRaven0405'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present BlackRaven0405'
-__version__ = '0.0.3'
+__version__ = '1.0.0'
 
 
 from .board import *
